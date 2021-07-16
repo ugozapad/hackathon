9212c48d387fc1bfd9092922cd1808cbf2921ca9
Kirill Yurkin <hp1link2@gmail.com>
2021-07-16 23:23:33 +0300

    changed name

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -10,36 +10,36 @@ endif()
 
 function(set_target_name_ex TARGET NAME)
 	if (CMAKE_SIZEOF_VOID_P EQUAL 4 AND WIN32)
-		set(PLATFORM_STR "Win32")
+		set(PLATFORM_STR "win32")
 	elseif (WIN32)
-		set(PLATFORM_STR "Win64")
+		set(PLATFORM_STR "win64")
 	endif()
 	
 	set_target_properties(${TARGET} PROPERTIES
 		#OUTPUT_NAME "${NAME}_${PLATFORM_STR}_${PLATFORM_NAME}"
 		OUTPUT_NAME "${NAME}_${PLATFORM_STR}"
 	
-		DEBUG_POSTFIX "_Debug"
-		RELEASE_POSTFIX "_Release"
-		RELWITHDEBINFO_POSTFIX "_RelWithDebInfo"
-		MINSIZEREL_POSTFIX "_MinSizeRel"
+		DEBUG_POSTFIX "_d"
+		RELEASE_POSTFIX "_r"
+		RELWITHDEBINFO_POSTFIX "_rwdi"
+		MINSIZEREL_POSTFIX "_msr"
 	)
 endfunction()
 
 function(set_target_name TARGET)
 	if (CMAKE_SIZEOF_VOID_P EQUAL 4 AND WIN32)
-		set(PLATFORM_STR "Win32")
+		set(PLATFORM_STR "win32")
 	elseif (WIN32)
-		set(PLATFORM_STR "Win64")
+		set(PLATFORM_STR "win64")
 	endif()
 	
 	set_target_properties(${TARGET} PROPERTIES
 		OUTPUT_NAME "${TARGET}_${PLATFORM_STR}"
-	
-		DEBUG_POSTFIX "_Debug"
-		RELEASE_POSTFIX "_Release"
-		RELWITHDEBINFO_POSTFIX "_RelWithDebInfo"
-		MINSIZEREL_POSTFIX "_MinSizeRel"
+		
+		DEBUG_POSTFIX "_d"
+		RELEASE_POSTFIX "_r"
+		RELWITHDEBINFO_POSTFIX "_rwdi"
+		MINSIZEREL_POSTFIX "_msr"
 	)
 endfunction()
 
