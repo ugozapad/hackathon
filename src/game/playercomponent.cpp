69b4a4c87075f80619125f6defbc8b23ee82f6cc
Alivebyte <alivebyte.yt@gmail.com>
2021-08-02 21:14:19 +0300

    mouse controls

--- src/game/playercomponent.cpp
+++ src/game/playercomponent.cpp
@@ -35,8 +35,27 @@ namespace engine
 		Camera* cam = CameraProxy::getInstance();
 		InputManager* input = InputManager::getInstance();
 		glm::vec3 pos = m_node->getPosition();
-		float camSpeed = 10.0f * dt;
 		
+		float camSpeed = 10.0f * dt;
+		glm::vec2 mousePos = InputManager::getInstance()->getCursorPos();
+
+		float xoffset = mousePos.x;
+		float yoffset = mousePos.y;
+
+		xoffset *= 0.1f;
+		yoffset *= 0.1f;
+
+		float yaw = -90.0f;
+		float pitch = 0.0f;
+
+		yaw += xoffset;
+		pitch += yoffset;
+
+		if (pitch > 89.0f)
+			pitch = 89.0f;
+		if (pitch < -89.0f)
+			pitch = -89.0f;
+
 		if (input->getKey(GLFW_KEY_W))
 			pos += camSpeed * cam->m_direction;
 		if (input->getKey(GLFW_KEY_S))
@@ -48,9 +67,20 @@ namespace engine
 			pos += glm::normalize(glm::cross(cam->m_direction, glm::vec3(0.0f, 1.0f, 0.0f))) * camSpeed;
 
 		if (input->getKey(GLFW_KEY_Q))
-			pos.y += 10.0f * dt;
+			pos.y += camSpeed;
 		if (input->getKey(GLFW_KEY_Z))
-			pos.y -= 10.0f * dt;
+			pos.y -= camSpeed;
+
+
+		glm::vec3 front;
+		front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+		front.y = sin(glm::radians(pitch));
+		front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+		cam->m_direction = glm::normalize(front);
+		
+
+
+
 
 		cam->m_position = pos;
 		m_node->setPosition(pos);
