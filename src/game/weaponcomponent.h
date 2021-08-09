2af1c98b130060efe36ebf806d8c578cb1175625
Alivebyte <alivebyte.yt@gmail.com>
2021-08-09 22:50:10 +0300

    WeaponComponent

---
+++ src/game/weaponcomponent.h
@@ -0,0 +1,21 @@
+#ifndef WEAPONCOMPONENT_H
+#define WEAPONCOMPONENT_H
+
+
+#include "engine/components/logiccomponent.h"
+
+namespace engine {
+	class WeaponComponent : public LogicComponent
+	{
+		ImplementObject(WeaponComponent, LogicComponent);
+	public:
+		WeaponComponent();
+		~WeaponComponent();
+
+		static void registerObject();
+
+		
+		virtual void update(float dt);
+	};
+}
+#endif // ! WEAPONCOMPONENT_H
