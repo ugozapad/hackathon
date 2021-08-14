1cad8232cfd454cbcab1df211feef4249a645b2b
Kirill Yurkin <hp1link2@gmail.com>
2021-08-14 20:13:07 +0300

    Language Manager

--- src/game/languagemanager.cpp
+++ src/game/languagemanager.cpp
@@ -1,1 +1,51 @@
 #include "pch.h"
+#include "game/languagemanager.h"
+
+namespace engine
+{
+	LanguageManager LanguageManager::ms_instance;
+
+	enum class Languages
+	{
+		English,
+		Russian
+	};
+
+	Languages getPreferedLanguage()
+	{
+		if (strstr(GetCommandLineA(), "-lang_en"))
+			return Languages::English;
+		else if (strstr(GetCommandLineA(), "-lang_ru"))
+			return Languages::Russian;
+
+		WORD language = PRIMARYLANGID(GetSystemDefaultLangID());
+		if (language == LANG_RUSSIAN)
+			return Languages::Russian;
+		
+		// Default
+		return Languages::English;
+	}
+
+	void LanguageManager::init()
+	{
+		Languages lang = getPreferedLanguage();
+
+
+	}
+
+	void LanguageManager::shutdown()
+	{
+
+	}
+
+	std::string LanguageManager::getAsciiStr(const std::string& langStrName)
+	{
+		throw std::logic_error("LanguageManager::getAsciiStr: not implemented");
+	}
+
+	std::wstring LanguageManager::getWideStr(const std::string& langStrName)
+	{
+		throw std::logic_error("LanguageManager::getWideStr: not implemented");
+	}
+
+}
---
+++ src/game/languagemanager.h
@@ -0,0 +1,18 @@
+#ifndef LANGUAGEMANAGER_H
+#define LANGUAGEMANAGER_H
+
+namespace engine
+{
+	class LanguageManager : public Singleton<LanguageManager>
+	{
+		static LanguageManager ms_instance;
+	public:
+		void init();
+		void shutdown();
+
+		std::string getAsciiStr(const std::string& langStrName);
+		std::wstring getWideStr(const std::string& langStrName);
+	};
+}
+
+#endif // !LANGUAGEMANAGER_H
